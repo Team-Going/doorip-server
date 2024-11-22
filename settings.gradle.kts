6acd86930988e928e43e9b273415b262f6d86561
sunwoong <sunwoong1122@gmail.com>
2024-11-22 17:38:46 +0900

    chore: ci & ktlint & pr template setting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,7 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
 include(
     "presentation:api",
     "core",
     "domain",
-    "gateway",
-    "support:yaml"
+    "gateway:rdb",
+    "support:yaml",
 )
-include("gateway:rdb")
-findProject(":gateway:rdb")?.name = "rdb"
