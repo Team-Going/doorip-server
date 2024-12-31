8e13641a566dbd32cedda12d475c026c4ca6c0b4
sunwoong <sunwoong1122@gmail.com>
2024-12-31 18:21:10 +0900

    #14 refactor: 코드 리뷰 반영

--- gateway/oauth/src/main/kotlin/org/doorip/gateway/oauth/config/OpenFeignConfig.kt
+++ gateway/oauth/src/main/kotlin/org/doorip/gateway/oauth/config/OpenFeignConfig.kt
@@ -3,6 +3,6 @@ package org.doorip.gateway.oauth.config
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Configuration
 
-@EnableFeignClients(basePackages = ["org.doorip"])
+@EnableFeignClients(basePackages = ["org.doorip.gateway.oauth"])
 @Configuration
 internal class OpenFeignConfig
