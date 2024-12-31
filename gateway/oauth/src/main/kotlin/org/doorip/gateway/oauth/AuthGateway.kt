dc3a1d22266d8b927fd785a29a07191aeeeebf89
sunwoong <sunwoong1122@gmail.com>
2024-12-31 18:02:13 +0900

    #14 feat: auth gateway 로직 구현

--- gateway/oauth/src/main/kotlin/org/doorip/gateway/oauth/AuthGateway.kt
+++ gateway/oauth/src/main/kotlin/org/doorip/gateway/oauth/AuthGateway.kt
@@ -2,14 +2,18 @@ package org.doorip.gateway.oauth
 
 import org.doorip.domain.entity.AuthPlatform
 import org.doorip.domain.repository.AuthRepository
+import org.doorip.gateway.oauth.apple.AppleAuthService
+import org.doorip.gateway.oauth.kakao.KakaoAuthService
 import org.springframework.stereotype.Component
 
 @Component
-internal class AuthGateway : AuthRepository {
+internal class AuthGateway(
+    private val appleAuthService: AppleAuthService,
+    private val kakaoAuthService: KakaoAuthService,
+) : AuthRepository {
 
-    override fun getPlatformId(token: String, platform: AuthPlatform): String {
-        // TODO
-
-        return token
+    override fun getPlatformId(token: String, platform: AuthPlatform): String = when (platform) {
+        AuthPlatform.APPLE -> appleAuthService.getPlatformId(token)
+        AuthPlatform.KAKAO -> kakaoAuthService.getPlatformId(token)
     }
 }
