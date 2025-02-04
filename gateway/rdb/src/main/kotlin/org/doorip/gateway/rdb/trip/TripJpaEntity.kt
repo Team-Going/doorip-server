ff257534e0fab5e5019d4f5a01cd4aef0100a4e8
sunwoong <sunwoong1122@gmail.com>
2025-02-04 23:25:54 +0900

    #22 feat: 참여자 & 여행 jpa 연관관계 관련 로직 추가

--- gateway/rdb/src/main/kotlin/org/doorip/gateway/rdb/trip/TripJpaEntity.kt
+++ gateway/rdb/src/main/kotlin/org/doorip/gateway/rdb/trip/TripJpaEntity.kt
@@ -8,6 +8,8 @@ import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import java.time.LocalDate
+import org.doorip.domain.trip.Trip
+import org.doorip.domain.trip.TripId
 import org.hibernate.annotations.Cascade
 import org.hibernate.annotations.CascadeType
 
@@ -43,4 +45,34 @@ internal class TripJpaEntity {
     @Cascade(CascadeType.REMOVE)
     @OneToMany(mappedBy = "trip")
     val todos: MutableList<TodoJpaEntity> = mutableListOf()
+
+    internal fun addParticipant(participant: ParticipantJpaEntity) {
+        participants.add(participant)
+    }
+
+    internal fun removeParticipant(participant: ParticipantJpaEntity) {
+        participants.remove(participant)
+    }
+
+    companion object {
+        fun of(code: String, title: String, startDate: LocalDate, endDate: LocalDate): TripJpaEntity {
+            return TripJpaEntity().apply {
+                this.code = code
+                this.title = title
+                this.startDate = startDate
+                this.endDate = endDate
+            }
+        }
+    }
+}
+
+internal fun TripJpaEntity.toDomain(): Trip {
+    return Trip(
+        id = TripId(this.id),
+        code = this.code,
+        title = this.title,
+        startAt = this.startDate,
+        endAt = this.endDate,
+        participants = participants.map { it.toDomain() },
+    )
 }
