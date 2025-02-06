6fdfc4ef88201f46f4a8de589d0ce300388aa473
sunwoong <sunwoong1122@gmail.com>
2025-02-06 16:59:05 +0900

    #24 feat: 여행 입장 & 초대코드 검증 gateway 로직 구현

--- domain/src/main/kotlin/org/doorip/domain/trip/TripRepository.kt
+++ domain/src/main/kotlin/org/doorip/domain/trip/TripRepository.kt
@@ -5,4 +5,8 @@ import org.doorip.domain.user.UserId
 
 interface TripRepository {
     fun createTrip(userId: UserId, title: String, startAt: LocalDate, endAt: LocalDate, styles: PropensityTag): Trip
+    fun verifyTripInvitation(code: String): Trip?
+    fun entryTrip(userId: UserId, tripId: TripId, styles: PropensityTag)
+    fun getTrip(tripId: TripId): Trip?
+    fun validateIsValidTrip(tripId: TripId)
 }
--- gateway/rdb/src/main/kotlin/org/doorip/gateway/rdb/trip/TripGateway.kt
+++ gateway/rdb/src/main/kotlin/org/doorip/gateway/rdb/trip/TripGateway.kt
@@ -1,21 +1,33 @@
 package org.doorip.gateway.rdb.trip
 
 import java.time.LocalDate
+import org.doorip.domain.AlreadyExistingParticipantException
+import org.doorip.domain.ExceedMaximumParticipantException
+import org.doorip.domain.TripNotFoundException
 import org.doorip.domain.UserNotFoundException
 import org.doorip.domain.trip.PropensityTag
 import org.doorip.domain.trip.Trip
+import org.doorip.domain.trip.TripId
 import org.doorip.domain.trip.TripRepository
 import org.doorip.domain.user.UserId
-import org.doorip.gateway.rdb.user.UserJpaRepository
+import org.doorip.gateway.rdb.trip.entity.ParticipantJpaEntity
+import org.doorip.gateway.rdb.trip.entity.TripJpaEntity
+import org.doorip.gateway.rdb.trip.entity.toDomain
+import org.doorip.gateway.rdb.trip.entity.toEmbeddable
+import org.doorip.gateway.rdb.trip.repository.ParticipantJpaRepository
+import org.doorip.gateway.rdb.trip.repository.TripJpaRepository
+import org.doorip.gateway.rdb.user.repository.UserJpaRepository
 import org.doorip.support.utils.RandomCodeGenerator
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
+@Transactional(readOnly = true)
 @Component
 internal class TripGateway(
     private val userJpaRepository: UserJpaRepository,
     private val tripJpaRepository: TripJpaRepository,
+    private val participantJpaRepository: ParticipantJpaRepository,
 ) : TripRepository {
 
     @Transactional
@@ -40,6 +52,47 @@ internal class TripGateway(
         return savedTrip.toDomain()
     }
 
+    override fun verifyTripInvitation(code: String): Trip? {
+        val trip = tripJpaRepository.findByCode(code) ?: return null
+
+        return trip.toDomain()
+    }
+
+    @Transactional
+    override fun entryTrip(userId: UserId, tripId: TripId, styles: PropensityTag) {
+        val user = userJpaRepository.findByIdOrNull(userId.value) ?: throw UserNotFoundException
+
+        val trip = tripJpaRepository.findByIdOrNull(tripId.value) ?: throw TripNotFoundException
+
+        if (participantJpaRepository.existsByUserIdAndTripId(userId.value, tripId.value)) {
+            throw AlreadyExistingParticipantException
+        }
+
+        val participant = ParticipantJpaEntity.of(
+            propensityTag = styles.toEmbeddable(),
+            user = user,
+            trip = trip,
+        )
+
+        participantJpaRepository.save(participant)
+    }
+
+    override fun getTrip(tripId: TripId): Trip? {
+        val trip = tripJpaRepository.findByIdOrNull(tripId.value) ?: return null
+
+        return trip.toDomain()
+    }
+
+    override fun validateIsValidTrip(tripId: TripId) {
+        val trip = tripJpaRepository.findByIdOrNull(tripId.value) ?: throw TripNotFoundException
+
+        val participants = trip.participants
+
+        if (participants.size >= MAXIMUM_PARTICIPANT) {
+            throw ExceedMaximumParticipantException
+        }
+    }
+
     private fun generateCode(): String {
         var code = RandomCodeGenerator.generateRandomCode()
 
@@ -48,5 +101,9 @@ internal class TripGateway(
         }
 
         return code
+    }
+
+    companion object {
+        private const val MAXIMUM_PARTICIPANT = 6
     }
 }
