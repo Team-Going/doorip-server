aec90d8a58595a4b7a13fec02e1de69335496908
sunwoong <sunwoong1122@gmail.com>
2025-02-06 16:59:23 +0900

    #24 feat: 여행 입장 & 초대코드 검증 비즈니스 로직 구현

--- core/src/main/kotlin/org/doorip/core/trip/TripUseCase.kt
+++ core/src/main/kotlin/org/doorip/core/trip/TripUseCase.kt
@@ -3,8 +3,11 @@ package org.doorip.core.trip
 import java.time.LocalDate
 import org.doorip.domain.trip.PropensityTag
 import org.doorip.domain.trip.Trip
+import org.doorip.domain.trip.TripId
 import org.doorip.domain.user.UserId
 
 interface TripUseCase {
     fun createTrip(userId: UserId, title: String, startAt: LocalDate, endAt: LocalDate, styles: PropensityTag): Trip
+    fun verifyInvitationCode(code: String): Trip
+    fun entryTrip(userId: UserId, tripId: TripId, styles: PropensityTag): TripId
 }
